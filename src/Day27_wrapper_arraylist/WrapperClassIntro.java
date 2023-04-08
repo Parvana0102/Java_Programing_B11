c414b03c7e6849d5594a31af26b121250432dc16
Parvana Shamkhalova <parvana_shamkhalova2014@gmail.com>
2023-04-08 14:13:18 -0500

    Day 27 work Wrapper classes

---
+++ src/Day27_wrapper_arraylist/WrapperClassIntro.java
@@ -0,0 +1,13 @@
+package Day27_wrapper_arraylist;
+
+public class WrapperClassIntro {
+
+        public static void main(String[] args) {
+
+            System.out.println(    Character.isUpperCase('A')    );
+            System.out.println(    Character.isLowerCase('A')    );
+
+        }
+
+    }
+
